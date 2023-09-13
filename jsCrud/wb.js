285066101db230976b598e8dbb55d3b2c470d762
Daniellllracz <78079867+Daniellllracz@users.noreply.github.com>
2023-09-13 10:25:13 +0200

    Add files via upload

---
+++ jsCrud/wb.js
@@ -0,0 +1,25 @@
+let allapot = {
+    csokik:[
+        {
+            nev: "Etcsoki",
+            ara: 2500,
+            raktaron:false
+        },
+        {
+            nev: "Tejcsoki",
+            ara: 1500,
+            raktaron:true
+        },
+        {
+            nev: "Fehercsoki",
+            ara: 4500,
+            raktaron:true
+        }
+    ]
+};
+function renderCsokik() {
+    let csokiHTML = "";
+    for (const csoki of allapot.csokik) {
+        
+    }
+}
